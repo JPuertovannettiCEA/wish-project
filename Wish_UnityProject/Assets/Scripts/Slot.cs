bdd178513e2cb57cae4199b6d513ce0c29945e54
Jorge <gd04jorge@thecea.ca>
2022-10-23 15:13:46 -0700

    Updated Inventory with more items
    Now you can erase items from your inventory

---
+++ Wish_UnityProject/Assets/Scripts/Slot.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slot : MonoBehaviour
+{
+    public int i;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if(transform.childCount <= 0)
+        {
+            GameManager.instance.inventoryisFull[i] = false;
+        }
+    }
+
+    public void DropItem()
+    {
+        foreach(Transform child in transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+    }
+}
